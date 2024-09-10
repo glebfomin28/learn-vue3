652070d3c8af177cebb81850837d186f40063291
Gleb Fomin <gleb.fom28@gmail.com>
2024-09-10 22:26:40 +0500

    init

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,15 @@
+<script setup>
+import Hello from './components/Hello.vue';
+</script>
+
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <Hello title="Title 1" content="Text" />
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 </style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,21 @@
+<script setup>
+defineProps({
+  title: String,
+  content: Number
+});
+</script>
+
+<template>
+  <div class="card">
+    <h2>{{ title }}</h2>
+    <div>{{ content }}</div>
+    <!--    <div>{{ // card?.id }}</div>-->
+  </div>
+</template>
+
+<style scoped>
+.card {
+  padding: 10px;
+  background: #a2a6f8;
+}
+</style>
